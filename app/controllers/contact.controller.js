8c860fdbf7b0a1829e98384b33887f6b7d1f869f
nartrung <nartrung02@gmail.com>
2023-02-17 12:52:27 +0700

    Fixed wrong route

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,21 +1,21 @@
 exports.create = (req,res) =>{
-    res.json({ message: "create handle" });
+    res.json({ message: "create handle"});
 };
 exports.findAll = (req,res) =>{
-    res.json({ message: "findAll handle" });
+    res.json({ message: "findAll handle"});
 };
 exports.findOne = (req,res) =>{
-    res.json({ message: "findOne handle" });
+    res.json({ message: "findOne handle"});
 };
 exports.findAllFavorite = (req,res) =>{
-    res.json({ message: "findAllFavorite handle" });
+    res.json({ message: "findAllFavorite handle"});
 };
 exports.delete = (req,res) =>{
-    res.json({ message: "delete handle" });
+    res.json({ message: "delete handle"});
 };
 exports.deleteAll = (req,res) =>{
-    res.json({ message: "deleteAll handle" });
+    res.json({ message: "deleteAll handle"});
 };
 exports.update = (req,res) =>{
-    res.json({ message: "update handle" });
+    res.json({ message: "update handle"});
 };
--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -3,13 +3,13 @@ const contacts = require('../controllers/contact.controller');
 
 const router = express.Router();
 
+router.route("/favorite")
+    .get(contacts.findAllFavorite);
+
 router.route("/:id")
     .get(contacts.findOne)
     .put(contacts.update)
     .delete(contacts.delete)
-
-router.route("/favorite")
-    .get(contacts.findAllFavorite);
 
 router.route("/")
     .get(contacts.findAll)
